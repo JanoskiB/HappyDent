01b2964fbd52a98199821c047918231dcaa08cf0
lillapedersen <10111612@ucn.dk>
2023-01-10 19:22:30 +0100

    Staff section fixed on About us page

--- assets/script/script.js
+++ assets/script/script.js
@@ -131,3 +131,21 @@ form.addEventListener("submit", function (e) {
 
 // Contact form
 
+// Read more button & functionality
+
+function myFunction() {
+   var dots = document.getElementById("dots");
+   var moreText = document.getElementById("more");
+   var btnText = document.getElementById("myBtn");
+ 
+   if (dots.style.display === "none") {
+     dots.style.display = "inline";
+     btnText.innerHTML = "Read more"; 
+     moreText.style.display = "none";
+   } else {
+     dots.style.display = "none";
+     btnText.innerHTML = "Read less"; 
+     moreText.style.display = "inline";
+   }
+ }
+
